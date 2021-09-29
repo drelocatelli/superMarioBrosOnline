ace172f5839f5ab8b6353e4f0603ce82270c5bd4
Your Name <you@example.com>
2021-09-28 23:42:09 -0300

    add server

--- public/assets/js/game_server.js
+++ public/assets/js/game_server.js
@@ -2,9 +2,9 @@ const socket = io()
 
 let connectionsEl = document.querySelector('connections')
 
-let playersEl = document.querySelector('.players') 
+let players = document.querySelector('.players') 
 
-let player = [
+let character = [
     "Mario",
     "Luigi",
     "Yoshi"
@@ -14,12 +14,12 @@ socket.on('login', action =>{
     connectionsEl.querySelector('li#users').innerHTML = `<li>${action.users} player(s)</li>` 
 
     // player elements
-    let players = new Array(parseInt(action.users))
+    let player = new Array(parseInt(action.users))
 
     // seta player na tela
 
     for(let i = 0; i < parseInt(action.users); i++){
-        players[i] = `<div class="player-container" data-id="${action.id}">
+        player[i] = `<div class="player-container" data-id="${action.id}">
                  <span class="name">Player ${i+1}</span>
                 
                  <img src="../public/assets/sprites/mario-1.png" class="player">
@@ -27,8 +27,8 @@ socket.on('login', action =>{
              `
     }
 
-    players.forEach(player => {
-        playersEl.innerHTML += player
+    player.forEach(player => {
+        players.innerHTML += player
     })
     
 })
@@ -38,7 +38,7 @@ socket.on('logout', action =>{
 
         connectionsEl.querySelector('li#users').innerHTML = `<li>${action.users} player(s)</li>` 
 
-        let playersContainers = playersEl.querySelectorAll('.player-container')
+        let playersContainers = players.querySelectorAll('.player-container')
         
         playersContainers.forEach(playerContainer => {
             if(playerContainer.dataset.id == action.id){
